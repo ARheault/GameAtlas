b282c99e128e311a0e4a4cd6b65b4f120163afc4
AlexRheault <alexdrhlt@gmail.com>
2021-05-21 09:42:07 -0700

    Building skeleton for backend game routes

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
-const Game = require("../models/location.model")
+const Game = require("../models/location.model");
 
 // Default route returns all users
 router.route("/").get((req, res) => {
-    Game.find()
-      .then((games) => res.json(games))
-      .catch((err) => res.status(400).send(`Error: ${err}`));
-  });
+  Game.find()
+    .then((games) => res.json(games))
+    .catch((err) => res.status(400).send(`Error: ${err}`));
+});
+
+router.route("/add").post((req, res) => {
+  console.log("add route for games");
+});
+
+router.route("/delete").post((req, res) => {
+  console.log("delete route for games");
+});
+
+router.route("/findGame").post((req, res) => {
+  console.log("Find a game based on request.");
+});
+
+router.route("/findGame:id").get((req, res) => {
+  console.log("Find a game based on ID passed via url");
+});
+
+module.exports = router;
