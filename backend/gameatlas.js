2820b682361cdf92a96cf5011f5b0f91a7a3bb93
AlexRheault <alexdrhlt@gmail.com>
2021-05-22 14:27:42 -0700

    Clarity for users models and their routes

--- backend/gameatlas.js
+++ backend/gameatlas.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://OurTeam:OurPass@cluster0.osqu2.mongodb.net/GameAtlasDB?";
+const uri = process.env.ATLAS_URI;
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
