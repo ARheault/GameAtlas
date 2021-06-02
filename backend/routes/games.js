8ad2210a28dc4c907147468d33360baf11dbe1ed
AlexRheault <alexdrhlt@gmail.com>
2021-06-01 19:30:40 -0700

    closes #9, closes #10; Implementation of find game routes done for json and id requests

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -38,39 +38,47 @@ router.route("/add").post(async (req, res) => {
 });
 
 router.route("/delete").post(async (req, res) => {
- // First authentification to make sure the client should be allowed to delete this account.
- const aGame = await Game.find({ gameName: req.body.name });
- if (aGame.length) {
-   // query for user to delete
-   const query = { gameName: req.body.name };
+  // First authentification to make sure the client should be allowed to delete this account.
+  const aGame = await Game.find({ gameName: req.body.name });
+  if (aGame.length) {
+    // query for user to delete
+    const query = { gameName: req.body.name };
 
-   Game.deleteOne(query, (err, result) => {
-     if (err) {
-       res.send(
-         `Error when attempting to delete user: ${req.body.name}\nError: ${err}`
-       );
-     } else {
-       res.status(200).json({ success: true, userDeleted: req.body.name });
-     }
-   });
- } else {
-   console.log("Game does not exist.");
-   res
-     .status(406)
-     .json({ authenticated: false, reason: "Game not found" });
- }
+    Game.deleteOne(query, (err, result) => {
+      if (err) {
+        res.send(
+          `Error when attempting to delete user: ${req.body.name}\nError: ${err}`
+        );
+      } else {
+        res.status(200).json({ success: true, userDeleted: req.body.name });
+      }
+    });
+  } else {
+    console.log("Game does not exist.");
+    res.status(406).json({ authenticated: false, reason: "Game not found" });
+  }
 });
 
-router.route("/findGame").post((req, res) => {
-  console.log("Find a game based on request.");
+router.route("/findGame").post(async (req, res) => {
+  const aGame = await Game.find({ gameName: req.body.name });
+  if (aGame.length) {
+    res.status(200).json({ suceess: true, game: aGame[0] });
+  } else {
+    res.status(400).json({ success: false, reason: "Game not found" });
+  }
 });
 
-router.route("/findGame:id").get((req, res) => {
-  console.log("Find a game based on ID passed via url");
+router.route("/findGame:id").get(async (req, res) => {
+  const aGame = await Game.find({ gameName: req.params.id });
+  if (aGame.length) {
+    res.status(200).json({ suceess: true, game: aGame[0] });
+  } else {
+    res.status(400).json({ success: false, reason: "Game not found" });
+  }
 });
 
 router.route("/edit").post((req, res) => {
-  console.log("Edit a game based on request.")
+  console.log("Edit a game based on request.");
 });
 
 router.route("/addAuthor").post((req, res) => {
