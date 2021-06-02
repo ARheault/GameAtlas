46357c174cd676aad1ccc88b7fb6008eac45b0f9
AlexRheault <alexdrhlt@gmail.com>
2021-06-02 08:27:43 -0700

    refactored deleteGame route to reduce complexity

--- backend/routes/locations.js
+++ backend/routes/locations.js
@@ -85,60 +85,55 @@ router.route("/deleteGame").post(async (req, res) => {
   const aLocation = await Location.find({ name: req.body.name });
 
   // Make sure location exists
-  if (aLocation.length) {
-    // Check if game we want to delete is in this location
-    if (aLocation[0].games.includes(req.body.gameToDelete)) {
-      // Filter out the game
-      aLocation[0].games = aLocation[0].games.filter((game) => {
-        if (game !== req.body.gameToDelete) {
-          return game;
-        }
-      });
-
-      // If the location still has games
-      if (aLocation[0].games.length > 0) {
-        --aLocation[0].numGames;
-        aLocation[0]
-          .save()
-          .then(() =>
-            console.log(
-              `Location ${req.body.name} deleted game ${req.body.gameToDelete}`
-            )
-          )
-          .then(() =>
-            res.status(200).json({
-              Location: req.body.name,
-              GameDeleted: req.body.gameToDelete,
-            })
-          )
-          .catch((err) =>
-            res.status(400).json({ Error: err, Location: req.body.name })
-          );
-      }
-
-      // Otherwise if it has no games anymore delete the empty location
-      else {
-        const query = { name: req.body.name };
-
-        Location.deleteOne(query, (err, result) => {
-          if (err) {
-            res.send(
-              `Error when attempting to delete location: ${req.body.name}\nError: ${err}`
-            );
-          } else {
-            res
-              .status(200)
-              .json({ success: true, locationDeleted: req.body.name });
-          }
-        });
-      }
-    } else {
-      res
-        .status(400)
-        .json({ success: false, reason: "Game not found in location" });
-    }
-  } else {
+  if (!aLocation.length) {
     res.status(400).json({ success: false, reason: "Location not found" });
+  }
+  // Check if game we want to delete is in this location
+  if (!aLocation[0].games.includes(req.body.gameToDelete)) {
+    res
+      .status(400)
+      .json({ success: false, reason: "Game not found in location" });
+  }
+  // Filter out the game
+  aLocation[0].games = aLocation[0].games.filter((game) => {
+    if (game !== req.body.gameToDelete) {
+      return game;
+    }
+  });
+  --aLocation[0].numGames;
+  // If the location still has games
+  if (aLocation[0].games.length > 0) {
+    aLocation[0]
+      .save()
+      .then(() =>
+        console.log(
+          `Location ${req.body.name} deleted game ${req.body.gameToDelete}`
+        )
+      )
+      .then(() =>
+        res.status(200).json({
+          Location: req.body.name,
+          GameDeleted: req.body.gameToDelete,
+        })
+      )
+      .catch((err) =>
+        res.status(400).json({ Error: err, Location: req.body.name })
+      );
+  }
+
+  // Otherwise if it has no games anymore delete the empty location
+  else {
+    const query = { name: req.body.name };
+
+    Location.deleteOne(query, (err, result) => {
+      if (err) {
+        res.send(
+          `Error when attempting to delete location: ${req.body.name}\nError: ${err}`
+        );
+      } else {
+        res.status(200).json({ success: true, locationDeleted: req.body.name });
+      }
+    });
   }
 });
 
