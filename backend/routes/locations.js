d691701e640a098a5457f1eb9497ab5c4c61cf1a
AlexRheault <alexdrhlt@gmail.com>
2021-06-02 08:15:31 -0700

    closes #11 route for delete game from location implemented

--- backend/routes/locations.js
+++ backend/routes/locations.js
@@ -48,7 +48,7 @@ router.route("/delete").post(async (req, res) => {
           `Error when attempting to delete user: ${req.body.name}\nError: ${err}`
         );
       } else {
-        res.status(200).json({ success: true, userDeleted: req.body.name });
+        res.status(200).json({ success: true, locationDeleted: req.body.name });
       }
     });
   } else {
@@ -59,23 +59,21 @@ router.route("/delete").post(async (req, res) => {
   }
 });
 
-router.route("/findLocation").post( async (req, res) => {
-  const aLocation = await Location.find({name: req.body.name });
-  if(aLocation.length){
-    res.status(200).json({suceess: true, location: aLocation[0]});
-  }
-  else{
-    res.status(400).json({success: false, reason: "Location not found"});
+router.route("/findLocation").post(async (req, res) => {
+  const aLocation = await Location.find({ name: req.body.name });
+  if (aLocation.length) {
+    res.status(200).json({ suceess: true, location: aLocation[0] });
+  } else {
+    res.status(400).json({ success: false, reason: "Location not found" });
   }
 });
 
 router.route("/findLocation:id").get(async (req, res) => {
-  const aLocation = await Location.find({name: req.params.id});
-  if(aLocation.length){
-    res.status(200).json({suceess: true, location: aLocation[0]});
-  }
-  else{
-    res.status(400).json({success: false, reason: "Location not found"});
+  const aLocation = await Location.find({ name: req.params.id });
+  if (aLocation.length) {
+    res.status(200).json({ suceess: true, location: aLocation[0] });
+  } else {
+    res.status(400).json({ success: false, reason: "Location not found" });
   }
 });
 
@@ -83,8 +81,56 @@ router.route("/addGame").post((req, res) => {
   console.log("add a game to a location based on request.");
 });
 
-router.route("/deleteGame").post((req, res) => {
-  console.log("Delete a game based on request.");
+router.route("/deleteGame").post(async (req, res) => {
+  const aLocation = await Location.find({ name: req.body.name });
+
+  // Make sure location exists
+  if (aLocation.length) {
+    
+    // Check if game we want to delete is in this location
+    if (aLocation[0].games.includes(req.body.gameToDelete)) {
+
+      // Filter out the game
+      aLocation[0].games = aLocation[0].games.filter((game) => {
+        if (game !== req.body.gameToDelete) {
+          return game;
+        }
+      });
+
+      // If the location still has games
+      if (aLocation[0].games.length > 0) {
+        --aLocation[0].numGames;
+        aLocation[0]
+          .save()
+          .then(() => console.log(`Location ${req.body.name} deleted game ${req.body.gameToDelete}`))
+          .then(() => res.status(200).json({Location: req.body.name, GameDeleted: req.body.gameToDelete}))
+          .catch((err) =>
+            res.status(400).json({ Error: err, Location: req.body.name })
+          );
+      } 
+
+      // Otherwise if it has no games anymore delete the empty location 
+      else {
+        const query = { name: req.body.name };
+
+        Location.deleteOne(query, (err, result) => {
+          if (err) {
+            res.send(
+              `Error when attempting to delete location: ${req.body.name}\nError: ${err}`
+            );
+          } else {
+            res.status(200).json({ success: true, locationDeleted: req.body.name });
+          }
+        });
+      }
+    } else {
+      res
+        .status(400)
+        .json({ success: false, reason: "Game not found in location" });
+    }
+  } else {
+    res.status(400).json({ success: false, reason: "Location not found" });
+  }
 });
 
 router.route("/addAuthor").post((req, res) => {
