49df772d2d0bea485364f2a6b8c50544f89813f8
AlexRheault <alexdrhlt@gmail.com>
2021-06-02 09:00:42 -0700

    closes #15 edit route implemented for the game model

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -19,11 +19,13 @@ router.route("/add").post(async (req, res) => {
     const minPlayers = req.body.min;
     const maxPlayers = req.body.max;
     const isReocurring = req.body.reocurring;
+    const creator = req.body.creator;
 
     const newGame = new Game({
       gameName: gameName,
       minimumPlayers: minPlayers,
       maximumPlayers: maxPlayers,
+      creators: creator,
       reocurring: isReocurring,
       dateCreated: Date.now(),
     });
@@ -41,16 +43,22 @@ router.route("/delete").post(async (req, res) => {
   // First authentification to make sure the client should be allowed to delete this account.
   const aGame = await Game.find({ gameName: req.body.name });
   if (aGame.length) {
+    if (!aGame[0].creators.includes(req.body.clientName)) {
+      res.status(400).json({
+        success: false,
+        reason: `client is requesting to delete game they do not have access to`,
+      });
+    }
     // query for user to delete
     const query = { gameName: req.body.name };
 
     Game.deleteOne(query, (err, result) => {
       if (err) {
         res.send(
-          `Error when attempting to delete user: ${req.body.name}\nError: ${err}`
+          `Error when attempting to delete game: ${req.body.name}\nError: ${err}`
         );
       } else {
-        res.status(200).json({ success: true, userDeleted: req.body.name });
+        res.status(200).json({ success: true, gameDeleted: req.body.name });
       }
     });
   } else {
@@ -77,32 +85,83 @@ router.route("/findGame:id").get(async (req, res) => {
   }
 });
 
-router.route("/edit").post((req, res) => {
-  console.log("Edit a game based on request.");
+router.route("/edit").post(async (req, res) => {
+  const query = { gameName: req.body.gameToEdit };
+  const aGame = await Game.find({ gameName: req.body.gameToEdit });
+  if (!aGame.length) {
+    res.status(400).json({ success: false, reason: "Game not found" });
+  } else {
+    if (aGame[0].creators && !aGame[0].creators.includes(req.body.clientName)) {
+      res.status(400).json({
+        success: false,
+        reason: `client is requesting to edit a game they do not have access to`,
+      });
+    }
+
+    const gameName = req.body.name;
+    const minPlayers = req.body.min;
+    const maxPlayers = req.body.max;
+    const isReocurring = req.body.reocurring;
+    let creator = [];
+    aGame[0].creators.forEach((elem) => {
+      creator.push(elem);
+    });
+
+    const newGame = new Game({
+      gameName: gameName,
+      minimumPlayers: minPlayers,
+      maximumPlayers: maxPlayers,
+      creators: creator,
+      reocurring: isReocurring,
+      dateCreated: Date.now(),
+    });
+    newGame
+      .save()
+      .then(() =>
+        console.log(`Game: ${req.body.gameToEdit} edited to ${req.body.name}.`)
+      )
+      .then(() => res.status(200).send(`Game: ${req.body.name} added.`))
+      .catch((err) =>
+        res.status(400).json({ Error: err, Game: req.body.name })
+      );
+
+    Game.deleteOne(query, (err, result) => {
+      if (err) {
+        res.send(
+          `Error when attempting to delete game: ${req.body.gameToEdit}\nError: ${err}`
+        );
+      }
+    });
+  }
 });
 
 router.route("/addAuthor").post(async (req, res) => {
   const aGame = await Game.find({ name: req.body.name });
 
-  if (aGame.length) {
-    aGame[0].creators.push(req.body.newCreator);
-    aGame[0]
-      .save()
-      .then(() =>
-        console.log(
-          `Game ${req.body.name} saved new creator ${req.body.newCreator}`
-        )
-      )
-      .then(() =>
-        res.status(200).json({
-          Game: req.body.name,
-          creatorAdded: req.body.newCreator,
-        })
-      )
-      .catch((err) =>
-        res.status(400).json({ Error: err, Game: req.body.name })
-      );
+  if (!aGame.length) {
+    res.status(400).json({ success: false, reason: "Game not found" });
   }
+  if (!aGame[0].creators.includes(req.body.clientName)) {
+    res.status(400).json({
+      success: false,
+      reason: `client is requesting to add an author to a game they do not have access to`,
+    });
+  }
+  aGame[0].creators.push(req.body.newCreator);
+  aGame[0]
+    .save()
+    .then(() =>
+      console.log(
+        `Game ${req.body.name} saved new creator ${req.body.newCreator}`
+      )
+    )
+    .then(() =>
+      res.status(200).json({
+        Game: req.body.name,
+        creatorAdded: req.body.newCreator,
+      })
+    )
+    .catch((err) => res.status(400).json({ Error: err, Game: req.body.name }));
 });
 
 module.exports = router;
