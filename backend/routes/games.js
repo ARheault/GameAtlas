b34e2b5b3f0042867d2946efd7ed635ff62653b9
AlexRheault <alexdrhlt@gmail.com>
2021-06-01 19:22:15 -0700

    closes #7 fixed import and implemented add game route.

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Game = require("../models/location.model");
+const Game = require("../models/game.model");
 
 // Default route returns all users
 router.route("/").get((req, res) => {
@@ -8,8 +8,33 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).send(`Error: ${err}`));
 });
 
-router.route("/add").post((req, res) => {
-  console.log("add route for games");
+router.route("/add").post(async (req, res) => {
+  // First make sure the client isn't attempting to add a game with a duplicate name
+  const aGame = await Game.find({ gameName: req.body.name });
+  if (aGame.length > 0) {
+    console.log("Game already exists.");
+    res.status(406).json({ success: false, reason: "Game already exists" });
+  } else {
+    const gameName = req.body.name;
+    const minPlayers = req.body.min;
+    const maxPlayers = req.body.max;
+    const isReocurring = req.body.reocurring;
+
+    const newGame = new Game({
+      gameName: gameName,
+      minimumPlayers: minPlayers,
+      maximumPlayers: maxPlayers,
+      reocurring: isReocurring,
+      dateCreated: Date.now(),
+    });
+    newGame
+      .save()
+      .then(() => console.log(`Game: ${req.body.name} added.`))
+      .then(() => res.status(200).send(`Game: ${req.body.name} added.`))
+      .catch((err) =>
+        res.status(400).json({ Error: err, Game: req.body.name })
+      );
+  }
 });
 
 router.route("/delete").post((req, res) => {
