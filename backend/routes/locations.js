9c837855630ea149a5aeefd7885336475822bcfb
AlexRheault <alexdrhlt@gmail.com>
2021-06-02 08:20:21 -0700

    closes #12 finished implementation for add author to location route

--- backend/routes/locations.js
+++ backend/routes/locations.js
@@ -86,10 +86,8 @@ router.route("/deleteGame").post(async (req, res) => {
 
   // Make sure location exists
   if (aLocation.length) {
-    
     // Check if game we want to delete is in this location
     if (aLocation[0].games.includes(req.body.gameToDelete)) {
-
       // Filter out the game
       aLocation[0].games = aLocation[0].games.filter((game) => {
         if (game !== req.body.gameToDelete) {
@@ -102,14 +100,23 @@ router.route("/deleteGame").post(async (req, res) => {
         --aLocation[0].numGames;
         aLocation[0]
           .save()
-          .then(() => console.log(`Location ${req.body.name} deleted game ${req.body.gameToDelete}`))
-          .then(() => res.status(200).json({Location: req.body.name, GameDeleted: req.body.gameToDelete}))
+          .then(() =>
+            console.log(
+              `Location ${req.body.name} deleted game ${req.body.gameToDelete}`
+            )
+          )
+          .then(() =>
+            res.status(200).json({
+              Location: req.body.name,
+              GameDeleted: req.body.gameToDelete,
+            })
+          )
           .catch((err) =>
             res.status(400).json({ Error: err, Location: req.body.name })
           );
-      } 
+      }
 
-      // Otherwise if it has no games anymore delete the empty location 
+      // Otherwise if it has no games anymore delete the empty location
       else {
         const query = { name: req.body.name };
 
@@ -119,7 +126,9 @@ router.route("/deleteGame").post(async (req, res) => {
               `Error when attempting to delete location: ${req.body.name}\nError: ${err}`
             );
           } else {
-            res.status(200).json({ success: true, locationDeleted: req.body.name });
+            res
+              .status(200)
+              .json({ success: true, locationDeleted: req.body.name });
           }
         });
       }
@@ -133,8 +142,28 @@ router.route("/deleteGame").post(async (req, res) => {
   }
 });
 
-router.route("/addAuthor").post((req, res) => {
-  console.log("Add an author to a location");
+router.route("/addAuthor").post(async (req, res) => {
+  const aLocation = await Location.find({ name: req.body.name });
+
+  if (aLocation.length) {
+    aLocation[0].creators.push(req.body.newCreator);
+    aLocation[0]
+      .save()
+      .then(() =>
+        console.log(
+          `Location ${req.body.name} saved new creator ${req.body.newCreator}`
+        )
+      )
+      .then(() =>
+        res.status(200).json({
+          Location: req.body.name,
+          creatorAdded: req.body.newCreator,
+        })
+      )
+      .catch((err) =>
+        res.status(400).json({ Error: err, Location: req.body.name })
+      );
+  }
 });
 
 module.exports = router;
